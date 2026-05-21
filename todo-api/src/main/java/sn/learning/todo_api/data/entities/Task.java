821628cffdf247e7d362bc76a45b9b699c454a41
KudzoNelsam <kudzo2004@gmail.com>
2026-05-21 15:46:56 +0000

    creation du projet

--- todo-api/src/main/java/sn/learning/todo_api/data/entities/Task.java
+++ todo-api/src/main/java/sn/learning/todo_api/data/entities/Task.java
@@ -1,5 +1,6 @@
 package sn.learning.todo_api.data.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -19,5 +20,6 @@ public class Task extends BaseEntity {
     private String description;
 
     @Enumerated(EnumType.STRING)
-    private Status status;
+    @Column(nullable = false)
+    private Status status = Status.ACTIVE;
 }
---
+++ todo-api/src/main/java/sn/learning/todo_api/seed/ApplicationSeed.java
@@ -0,0 +1,46 @@
+package sn.learning.todo_api.seed;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import sn.learning.todo_api.data.entities.Task;
+import sn.learning.todo_api.data.enums.Status;
+import sn.learning.todo_api.data.repository.TaskRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ApplicationSeed implements CommandLineRunner {
+    private final TaskRepository taskRepository;
+
+    @Override
+    public void run(String @NonNull ... args) throws Exception {
+        if (taskRepository.findAll().isEmpty()) {
+            List<Task> tasks = new ArrayList<>();
+
+            for (int i = 0; i < 10; i++) {
+                tasks.add(
+                        Task.builder()
+                            .title("Task " + i)
+                            .description("Description " + i)
+                            .build()
+                );
+            }
+            taskRepository.saveAll(tasks);
+            log.info("Task added successfully");
+        }else {
+            List<Task> tasks = taskRepository.findAll();
+            for (Task task : tasks) {
+                task.setStatus(Status.ACTIVE);
+            }
+            taskRepository.saveAll(tasks);
+            log.info("Task already added");
+        }
+        taskRepository.flush();
+    }
+}
